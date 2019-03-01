4a12c5bf47eee74b2f59073746428c1dab7ce616
Dominick <dxt9140@g.rit.edu>
2019-03-01 09:37:36 -0500

    Eh

--- MagicDice.py
+++ MagicDice.py
@@ -28,9 +28,7 @@ dice_like = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
-sobel = [[-1, -2, -1],
-		 [ 0,  0,  0],
-		 [ 1,  2,  1]]
+sobel = [[-1, -2, -1], [0,  0,  0],  [1,  2,  1]]
 
 
 def main():
@@ -49,34 +47,47 @@ def main():
 	print("IMAGE PATH: " + argl.image)
 
 	cv2.namedWindow("Magic Dice", cv2.WINDOW_NORMAL)
-	cv2.resizeWindow("Magic Dice", 1000, 1000)
+	cv2.resizeWindow("Magic Dice", 1000, 600)
 
 	# Preprocess the image to obtain desired dimensions
 	image = cv2.imread(argl.image)
-	image = cv2.resize(image, (1200, 1200))
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	image = cv2.resize(image, (1000, 600))
 
-	fixed = image
+	fixed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	# Apply a series of transformations to enhance the image
-	fixed = cv2.GaussianBlur(fixed, (3, 3), 0)
-	# Find the tiny little holes and make them bigger
-	fixed = cv2.erode( fixed, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)) )
-	# Further expand the holes
-	fixed = cv2.erode( fixed, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)) )
+	# fixed = cv2.GaussianBlur(fixed, (9, 9), 0)
+	fixed = cv2.medianBlur(fixed, 5)
 
-	fixed = cv2.dilate( fixed, cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9)) )
+	_, fixed = cv2.threshold(fixed, 130, 255, cv2.THRESH_BINARY)
+	# kernel = np.ones((3, 3), np.uint8)
+	# fixed = cv2.adaptiveThreshold(fixed, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 15)
+	# fixed = cv2.morphologyEx(fixed, cv2.MORPH_OPEN, kernel, iterations=1)
+	# fixed = cv2.equalizeHist(fixed)
 
-	#fixed = cv2.Canny(fixed, 50, 120)
+	stuff = cv2.connectedComponentsWithStats(fixed, connectivity=4, ltype=cv2.CV_32S)
+	num_labels = stuff[0]
+	labels = stuff[1]
+	stats = stuff[2]
+	centroids = stuff[3]
 
-	#sobelx = np.array(sobel)
-	#sobely = np.transpose(sobelx)
-	#fixed = signal.convolve2d(fixed, sobelx[::-1,::-1])
+	_, dots = cv2.threshold(fixed, 0, 255, cv2.THRESH_BINARY_INV)
 
-	#_, fixed = cv2.threshold(fixed, 80, 255, cv2.THRESH_BINARY)
-	# fixed = cv2.Canny(fixed, 50, 120)
-	# fixed = cv2.applyColorMap(fixed, cv2.COLORMAP_JET)
+	for cent in centroids:
+		cv2.rectangle(image, (int(cent[0]), int(cent[1])), (int(cent[0])+5, int(cent[1])+5), (0, 0, 255), 3)
 
-	both = np.hstack((image, fixed))
+	copy = fixed.copy()
+	_, contours, _ = cv2.findContours(copy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	huh = cv2.filter2D(fixed, 3, dots)
+
+	image = cv2.drawContours(image, contours, -1, (0, 255, 127), 2)
+	#for cnt in contours:
+		# area = cv2.contourArea(cnt)
+
+	fixed = cv2.cvtColor(fixed, cv2.COLOR_GRAY2BGR)
+	dots = cv2.cvtColor(dots, cv2.COLOR_GRAY2BGR)
+
+	both = np.hstack((image, fixed, dots, huh))
 	cv2.imshow("Magic Dice", both)
 	cv2.waitKey(0)
 
